b21d25c1488e4e2561f8cd59f1067cf6867d2b26
ghiles.ouhenia <g.ouhenia@gmail.com>
2023-05-04 19:12:08 +0200

    multi sport

--- src/utils/generation.py
+++ src/utils/generation.py
@@ -35,3 +35,34 @@ def generate_data_gauss(n_samples, means, sigmas, labels=None):
     np.random.shuffle(index)
 
     return X[index], y[index]
+
+
+def generate_echiquier(n_samples, sigmas, n = 4):
+
+    means = [[i, j] for i in range(1,n+1) for j in range(1,n+1)]
+    sig = [np.eye(2) * sigmas] * len(means)
+    labels = [0 if i%2 == j%2 else 1 for i in range(1,n+1) for j in range(1,n+1)]
+
+    # Génération des données
+    return generate_data_gauss(n_samples, means, sig, labels)
+
+def generate_data_sphere(n_samples, locs, classes):
+    X_list = []
+    y_list = []
+    
+    for cl, loc in zip(classes, locs):
+        theta = np.linspace(0, 2*np.pi, n_samples)
+        r = np.random.normal(loc=loc, scale=0.6, size=(n_samples,)) 
+        x = r * np.cos(theta)
+        y = r * np.sin(theta)
+
+        X_list.append(np.vstack((x, y)).T)
+        y_list.append([cl] * len(y))
+        
+    X = np.concatenate(X_list, axis=0)
+    y = np.concatenate(y_list, axis=0)
+
+    index = np.arange(len(X))
+    np.random.shuffle(index)
+
+    return X[index], y[index]
