9e42635a78acb891b0ccb98076f860a254f28e3c
Amayas29 <amayas29sd@gmail.com>
2023-05-08 09:17:00 +0200

    expr

--- src/utils/processing.py
+++ src/utils/processing.py
@@ -12,3 +12,20 @@ def one_hot_y(y, nb_classes):
     y_oh[np.arange(N), y_shift] = 1
 
     return y_oh
+
+
+def normalisation(data):
+    dt = data.copy()
+
+    for i in range(data.shape[1]):
+        mini = np.min(data[:, i])
+        maxi = np.max(data[:, i])
+        if maxi == mini:
+            if maxi > 0:
+                dt[:, i] = 1
+            else:
+                dt[:, i] = 0
+        else:
+            dt[:, i] = (data[:, i] - mini) / (maxi - mini)
+
+    return dt.astype("float64")
